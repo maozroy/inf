de90605da48e431a74ef3c8a41ec6ffb37dc41d6
maoz.roytman <maozroy@gmail.com>
2019-10-13 00:48:55 +0300

    final fixes to ws4

--- c/ws4.c
+++ c/ws4.c
@@ -1,6 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+/**************************/
+/*          WS4           */
+/* Author: Maoz Roytman   */
+/* Reviwer: Sharon Rottner*/
+/*        12/10/19        */
+/**************************/
 
+#include <stdio.h> /* for prints */
+#include <stdlib.h> /* for exit */
 
 void IfElse(char ch);
 void SwitchCase(char ch);
@@ -9,44 +15,22 @@ void esc();
 void akey();
 void tkey();
 void Empty();
-void (*lut_arr[256])(void) =  	{Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*0-9*/
-	  			 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*10-19*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, esc,Empty, Empty, 		/*20-29*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*30-39*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*40-49*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*50-59*/
-				 Empty, Empty, Empty, Empty, Empty, akey,Empty, Empty, Empty, Empty,  	/*60-69*/	
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*70-79*/
-				 Empty, Empty, Empty, Empty, tkey,Empty, Empty, Empty, Empty, Empty,   	/*80-89*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*90-99*/
-			 	 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*100-109*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*110-119*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*120-129*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*130-139*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*140-149*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*150-159*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*160-169*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*170-179*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*180-189*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*190-199*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, 		/*200-209*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,             /*210-219*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,             /*220-229*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,             /*230-239*/
-				 Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty,             /*240-249*/
-				 Empty, Empty, Empty, Empty, Empty, 0};                  /*250-255*/
+#define FIVEEmpty Empty, Empty, Empty, Empty, Empty
+#define TENEmpty FIVEEmpty, FIVEEmpty
+#define HUNDREDEmpty TENEmpty, TENEmpty, TENEmpty, TENEmpty, TENEmpty, \
+	TENEmpty, TENEmpty, TENEmpty, TENEmpty, TENEmpty
 							  			
 /*only activate one function at a time */
 int main ()
 {
-	char input = 0;
+	char input = '\0';
+
 	system("stty -icanon -echo");
 	input = getchar();
+	
 	/*SwitchCase(input);*/
 	IfElse(input);
 	/*LUT();*/
-
-
 
 	system("stty icanon echo");
 	return 0;
@@ -60,12 +44,17 @@ void Empty()
 
 void LUT()
 {
-	int ch = 0;
+	static void (*lut_arr[256])() = {TENEmpty, TENEmpty, FIVEEmpty, Empty, 
+		Empty, esc, TENEmpty, TENEmpty, TENEmpty, FIVEEmpty, Empty, 
+		Empty, akey, TENEmpty, FIVEEmpty, Empty, Empty, Empty, tkey,
+ 		HUNDREDEmpty, TENEmpty, TENEmpty, TENEmpty, TENEmpty, TENEmpty, 
+		TENEmpty, TENEmpty, Empty};
+	char ch = '\0';
 	
 	while(1)
 	{
 		ch = getchar();
-		(*lut_arr[(int) ch])();
+		(*lut_arr[(unsigned int) ch])();
 	}
 }
 
@@ -94,22 +83,18 @@ void IfElse(char ch)
 	while(1)
 	{
 		ch = getchar();
-		if(ch == 'A')
+		if('A' == ch)
 		{
 			printf("%c Pressed\n", ch);
 		}
-		if(ch == 'T')
+		if('T' == ch)
 		{
 			printf("%c Pressed\n", ch);
 		}
-		if(ch == 27)
+		if(27 == ch)
 		{
 			system("stty icanon echo");
 			return;
-		}
-		else
-		{
-			Empty();
 		}
 	}
 }
@@ -117,7 +102,6 @@ void IfElse(char ch)
 
 void SwitchCase(char ch)
 {
-	while(1)
 	{
 		ch = getchar();
 		switch(ch)
