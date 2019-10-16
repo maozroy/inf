7207d4f13d69da5d0159320208a82c142010c2d5
maoz.roytman <maozroy@gmail.com>
2019-10-17 00:11:07 +0300

    ws5 fix

--- c/ws5.c
+++ c/ws5.c
@@ -1,21 +1,32 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+/**************************/
+/*          WS5           */
+/* Author: Maoz Roytman   */
+/* Reviwer: ????          */
+/*        16/10/19        */
+/**************************/
 
-int op_remove(char *str ,char *file);
-int op_count(char *str ,char *file);
-int op_exit();
-int op_begin(char *str ,char *file);
-int op_write(char *str ,char *file);
-int comparing();
+#include <stdio.h>  /*FILE, EOF, prints, getc*/
+#include <string.h> /*strlen*/
+#include <stdlib.h> /*exit*/
+
+int op_remove(char *str ,char *file); /*removes the file*/
+int op_count(char *str ,char *file);  /*counts amount of lines in file*/
+int op_exit();					      /*exits the program*/
+int op_begin(char *str ,char *file);  /*writes to beginning of file*/
+int op_write(char *str ,char *file);  /*writes to file*/
+int comparing(); 					  /*compares input to string commands*/
+
 struct func_t
 {
-	char *op;
+	char *op;			
 	int (*op_func)();
 	int (*comparing)(char* str, char *function);
 };
 
-int main(int argc, char *argv[])
+/*when running the a.out, a declaration for a .txt file to output is needed.
+Maximum of 256 chars per line*/
+
+int main (int argc, char *argv[])
 {
 
 	struct func_t box[5] = {{"-remove", op_remove, comparing}, 
@@ -26,62 +37,59 @@ int main(int argc, char *argv[])
 	char input[256];
 	int i = 0;
 
-	
 	while(1)
 	{
-		fgets(input, 100, stdin);
+		fgets (input, 100, stdin);
 		do
 		{
-			if((box[i].comparing (input, box[i].op))==0)
+			if ((box[i].comparing (input, box[i].op))==0)
 			{
 				box[i].op_func(input, *(argv+1));
 			}
 
 		++i;
-		}while (i < 5 && ((box[i-1].comparing (input, box[i-1].op)) != 0));
+		} while (i < 5 && ((box[i-1].comparing (input, box[i-1].op)) != 0));
 		i = 0;
 	}
 	return 0;
 }
 
-
-
-
-int comparing(char* str, char *function)
+int comparing (char* str, char *function)
 {
 	return strncmp(function, str, strlen(function));
 }
 
-int op_write(char *str ,char *file)
+int op_write (char *str ,char *file)
 {
-
 	FILE *input;
+	
 	input = fopen(file, "a+");
 	fputs(str, input);
 	fclose(input);
 	return 0;
 }
 
-int op_remove(char *str ,char *file)
+int op_remove (char *str ,char *file)
 {
 	remove(file);
 	return 0;
 }
 
-int op_exit()
+int op_exit ()
 {
 	exit(0);
 	return 0;
 }
 
-int op_count(char *str ,char *file)
+int op_count (char *str ,char *file)
 {
 	char c = 0;
 	char counter = 0;
 	FILE *input;
+	
 	*str = 0;
 	input = fopen(file, "r");
-	for(c = getc(input) ; c!= EOF ; c = getc(input))
+	for (c = getc(input) ; c!= EOF ; c = getc(input))
 	{
 		if (c == '\n')
 		{
@@ -93,11 +101,11 @@ int op_count(char *str ,char *file)
 	return 0;
 }
 
-int op_begin(char *str ,char *file)
+int op_begin (char *str ,char *file)
 {
 	FILE *input;
+	
 	str++;
-	printf("this is op op_begin\n");
 	input = fopen(file, "r+");
 	fseek(input, 0L, SEEK_SET);
 	fputs(str, input);
