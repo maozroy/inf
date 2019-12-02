160f1e7806016fd438969b707d66c650661e6c5f
maoz.roytman <maozroy@gmail.com>
2019-12-02 21:31:33 +0200

    vsa stuff

--- ds/vsa/vsa.c
+++ ds/vsa/vsa.c
@@ -8,16 +8,19 @@
 
 #include <stdlib.h> /*MALLOCING*/
 #include <assert.h> /*asserting*/
+#include <stdio.h>
+
 
 #include "vsa.h"
 #define HEADER_SIZE sizeof(vsa_block_header_t)
 #define WORD_LENGTH sizeof(size_t)
 #define BLOCK vsa_block_header_t
 
+
 typedef struct block_header
 {
 	ssize_t block_size;
-	#ifdef DEBUG
+	#ifdef NDEBUG
     size_t magic_number;
     #endif
 }vsa_block_header_t;
@@ -26,7 +29,7 @@ size_t magic_number = 943579852543;
 
 static size_t WordAlignedIMP (size_t block_size);
 
-static BLOCK *FindClosestFreeSpaceIMP(vsa_t *vsa, size_t block_size);
+static BLOCK *FindClosestFreeSpaceIMP(vsa_t *vsa, ssize_t needed_space);
 
 /*sums space from block until next header, until block_size*/
 static size_t SumFreeSpaceIMP(BLOCK *block, size_t needed_space);
@@ -42,16 +45,15 @@ vsa_t *VSAInit(void *memory_pool, size_t pool_size)
 	assert(pool_size);
 	
 	vsa_begin = memory_pool;
-	end_header = (BLOCK *)((char *)memory_pool - HEADER_SIZE);
+	end_header = (BLOCK *)((char *)memory_pool + pool_size - HEADER_SIZE);
 	
 	vsa_begin->block_size = - pool_size + 2 * HEADER_SIZE;
 	end_header->block_size = 0;
 	
-	#ifdef DEBUG
+	#ifdef NDEBUG
 	vsa_begin->magic_number = magic_number;
 	end_header->magic_number = magic_number;
     #endif
-	
 	return vsa_begin;
 }
 
@@ -63,57 +65,165 @@ void *VSAAlloc(vsa_t *vsa, size_t block_size)
 	block_size = WordAlignedIMP(block_size);
 	
 	alloc_header = FindClosestFreeSpaceIMP(vsa, block_size);
+	if (alloc_header == NULL)
+	{
+		return NULL;
+	}
+	if (alloc_header->block_size == 0 || block_size == 0)
+	{
+		return NULL;
+	}
     
     next_header = (BLOCK *)((char *)alloc_header + block_size + HEADER_SIZE);
-    next_header_space = SumFreeSpaceIMP(alloc_header, block_size + HEADER_SIZE);
-    if (next_header_space < HEADER_SIZE)
-    {
-	 	/*don't write next header*/
-	 	/*add to block size the difference*/
-	}
-    next_header->block_size = SumFreeSpaceIMP(alloc_header, block_size + HEADER_SIZE);
+    next_header_space = SumFreeSpaceIMP(alloc_header, block_size);
     
-    alloc_header->block_size = block_size;
-
-	#ifdef DEBUG
-	next_header->magic_number = magic_number;
-    #endif
+    if (next_header_space <= (HEADER_SIZE*2 + block_size))
+    {
+ 	   alloc_header->block_size = next_header_space;
+	}
+	else
+	{
+	    alloc_header->block_size =  block_size;
+		next_header->block_size = -(next_header_space - 
+									block_size - HEADER_SIZE);
+		#ifdef NDEBUG
+		next_header->magic_number = magic_number;
+		#endif
+	}
     
     return (void *)((char *)alloc_header + HEADER_SIZE);
 }
 
-static BLOCK *FindClosestFreeSpaceIMP(vsa_t *vsa, size_t block_size)
+void VSAFree(void *allocated_address)
 {
+	BLOCK *block = (BLOCK *)((char *)allocated_address - HEADER_SIZE);
+	if (allocated_address == NULL)
+	{
+		return;
+	}
+	#ifdef NDEBUG
+	if (block->magic_number != magic_number)
+	{
+		return;
+	}
+	#endif
 	
+	block->block_size *= -1;
+}
 
+
+size_t VSALargestChunk(vsa_t *vsa)
+{
+	ssize_t counter = 0;
+	ssize_t max_counter = 0;
+	BLOCK *runner = NULL;
+	
+	runner = vsa;
+	
+	while (runner->block_size != 0)
+	{
+		if (runner->block_size < 0)
+		{
+			counter += abs(runner->block_size) + HEADER_SIZE;
+		}
+		else
+		{
+			if (counter > max_counter)
+			{
+				max_counter = counter;
+			}
+			counter = 0;
+		}
+		runner = NextBlockIMP(runner);
+	}
+	if (counter > max_counter)
+	{
+		max_counter = counter;
+	}
+	max_counter -= HEADER_SIZE;
+	if (max_counter < 0)
+	{
+		max_counter = 0;
+	}
+	return max_counter;
+}
+
+
+static BLOCK *FindClosestFreeSpaceIMP(vsa_t *vsa, ssize_t needed_space)
+{
+	ssize_t counter = 0;
+	ssize_t max_counter = 0;
+	BLOCK *out_runner = NULL;
+	BLOCK *inner_runner = NULL;
+	
+	out_runner = vsa;
+	while (out_runner->block_size != 0 && max_counter < needed_space)
+	{
+		inner_runner = out_runner;
+		
+		while ((inner_runner->block_size < 0) )
+		{
+			counter += abs(inner_runner->block_size) + HEADER_SIZE;
+			inner_runner = NextBlockIMP(inner_runner);	
+		}
+		if (counter > max_counter)
+		{
+			max_counter = counter;
+		}
+		
+		if (max_counter >= needed_space)
+		{
+			break;
+		}
+		
+		out_runner = NextBlockIMP(out_runner);
+		counter = 0;
+	}
+	
+	max_counter -= HEADER_SIZE;
+	if (max_counter < needed_space)
+	{
+		return NULL;
+	}
+	
+	
+	return out_runner;
 }
 
 static size_t SumFreeSpaceIMP(BLOCK *block, size_t needed_space)
 {
-	size_t free_space_counter = 0;
-	
-	while ((free_space_counter < needed_space) || (block->block_size < 0))
+	ssize_t free_space_counter = 0;
+	needed_space += HEADER_SIZE;
+	while ((free_space_counter < needed_space) && (block->block_size < 0))
 	{
-		free_space_counter += -1 * (block->block_size);
-		block = NextBlockIMP(block);	
+		free_space_counter += abs(block->block_size) + HEADER_SIZE;
+		block = NextBlockIMP(block);
+	}
+	free_space_counter -= HEADER_SIZE;
+	
+	if (free_space_counter < 0)
+	{
+		free_space_counter = 0;
 	}
 	
 	return free_space_counter;
+	/*(free_space_counter >= needed_space) ? free_space_counter : 0 ;*/
 
 }
 
 static BLOCK *NextBlockIMP(BLOCK *block)
 {
-	return (BLOCK *)((char *)block + block->block_size);
+	block = (BLOCK *)((char *)block + abs(block->block_size) + HEADER_SIZE);
+	return block;
 }
 
 static size_t WordAlignedIMP (size_t block_size)
 {
-	if (((block_size+HEADER_SIZE) % WORD_LENGTH) != 0)
+	if (((block_size) % WORD_LENGTH) != 0)
 	{
-		return ((block_size + HEADER_SIZE) / 
+		return ((block_size) / 
 				WORD_LENGTH ) * WORD_LENGTH + WORD_LENGTH;
 	}
-	
-	return (block_size + HEADER_SIZE) * WORD_LENGTH;
+
+	return block_size;
 }
--- ds/vsa/vsa_test.c
+++ ds/vsa/vsa_test.c
@@ -9,25 +9,102 @@ printf("Function: %-17sTest #%d  %s\n", \
 
 
 
-void TESTVSAInit (void);
+void TESTVSAInit(void);
+void TESTVSAAlloc(void);
+void TESTVSAFree(void);
 
 int main ()
 {
 	TESTVSAInit();
+	TESTVSAAlloc();
+	TESTVSAFree();
 	return 0;
 }
 
-void TESTVSAInit (void)
+void TESTVSAInit(void)
 {
-
-	size_t magic_number = 943579852543;
 	size_t memory_size = 150;
-	char *pool = malloc(memory_size);
+	void *pool = malloc(memory_size);
 	vsa_t *my_vsa = NULL;
-	my_vsa = VSAInit(pool, memory_size);
-	
-	#ifdef DEBUG
-	PRINT_TEST(*(size_t *)(pool + 8) == magic_number, "magic_test", 1);
-	#endif
 
+	my_vsa = VSAInit(pool, memory_size);
+
+	printf("INIT TEST\n");
+	#ifndef NDEBUG
+	PRINT_TEST(*(ssize_t *)((char *)pool) == -134, "test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 134) == 0, "test", 1);
+	#endif
+	#ifdef NDEBUG
+	PRINT_TEST(*(ssize_t *)((char *)pool) == -118, "test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 134) == 0, "test", 1);
+	PRINT_TEST(*(size_t *)((char *)pool + 8) == magic_number, "magic_test", 1);
+	PRINT_TEST(*(size_t *)((char *)pool + 142) == magic_number, "magic_test", 2);
+	#endif
+	free(my_vsa);
+
+}
+
+void TESTVSAAlloc(void)
+{
+	size_t memory_size = 150;
+	size_t magic_number = 943579852543;
+	void *pool = malloc(memory_size);
+	vsa_t *my_vsa = NULL;
+	void *header1 = NULL;
+	void *header2 = NULL;
+	void *header3 = NULL;
+	void *header4 = NULL;
+	
+	printf("ALLOC TEST\n");
+	my_vsa = VSAInit(pool, memory_size);
+	header1 = VSAAlloc(pool, 0);
+	PRINT_TEST(header1 == NULL, "0 alloc test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool) == -134, "0 alloc test", 2);
+	header2 = VSAAlloc(pool, 20);
+	PRINT_TEST(*(ssize_t *)((char *)pool) == 24, "first alloc test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 24 + 8) == -102, "first alloc test", 2);
+	header3 = VSAAlloc(pool, 25);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 32) == 32, "second alloc test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 32 + 40) == -62, "second alloc test", 2);
+	header4 = VSAAlloc(pool, 56);
+	PRINT_TEST(*(ssize_t *)((char *)header4 - 8) == 62, "third alloc test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 142) == 0, "third alloc test", 2);
+	
+	free(my_vsa);
+}
+
+void TESTVSAFree(void)
+{
+	size_t memory_size = 150;
+	size_t magic_number = 943579852543;
+	void *pool = malloc(memory_size);
+	vsa_t *my_vsa = NULL;
+	void *header1 = NULL;
+	void *header2 = NULL;
+	void *header3 = NULL;
+	void *header4 = NULL;
+	void *header5 = NULL;
+	void *header6 = NULL;
+	printf("FREE TEST\n");
+	my_vsa = VSAInit(pool, memory_size);
+	header1 = VSAAlloc(pool, 0);
+	VSAFree(header1);
+	PRINT_TEST(header1 == NULL, "NULL free test", 1);
+	header2 = VSAAlloc(pool, 20);
+	VSAFree(header2);
+	PRINT_TEST(*(ssize_t *)((char *)pool) == -24, "first free test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 24 + 8) == -102, "first free test", 2);
+	header2 = VSAAlloc(pool, 20);
+	header3 = VSAAlloc(pool, 25);
+	header4 = VSAAlloc(pool, 30);
+	VSAFree(header3);
+	PRINT_TEST(*(ssize_t *)((char *)header3 - 8) == -32, "second free test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)header3 + 32 ) == 32, "second free test", 2);
+	header3 = VSAAlloc(pool, 25);
+	header5 = VSAAlloc(pool, 25);
+	VSAFree(header3);
+	PRINT_TEST(*(ssize_t *)((char *)header4 - 8) == 62, "third alloc test", 1);
+	PRINT_TEST(*(ssize_t *)((char *)pool + 142) == 0, "third alloc test", 2);
+	
+	free(my_vsa);
 }
