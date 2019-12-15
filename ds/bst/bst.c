df44274f39065091c704fe59d49558c9b4fd12c6
maoz.roytman <maozroy@gmail.com>
2019-12-15 20:17:28 +0200

    bst

--- ds/bst/bst.c
+++ ds/bst/bst.c
@@ -36,11 +36,31 @@ struct bst
 	struct bst_node stub;
 };
 
-bst_node_t *CreateNodeIMP(bst_node_t *parent, void *data);
-bst_node_t *GetRightIMP(bst_node_t *node);
-bst_node_t *GetLeftIMP(bst_node_t *node);
-bst_node_t *GetParentIMP(bst_node_t *node);
-bst_iter_t GetRootIMP(bst_t *tree);
+enum kids_state {no_child = 0,
+				left_child = 1, 
+				right_child = 2, 
+				two_childs = 3};
+
+enum direction {up = 2,
+				left = -1, 
+				right = 1};
+				
+
+static bst_node_t *CreateNodeIMP(bst_node_t *parent, void *data);
+
+static bst_node_t *GetDirectionIMP(bst_node_t *node, enum direction direction);
+static bst_iter_t GetRootIMP(bst_t *tree);
+
+static int IsARightKidIMP(bst_iter_t node);
+
+static bst_iter_t BSTTraverseIMP(bst_iter_t iter, int sign);
+
+static enum kids_state GetNumOfKidsIMP(bst_iter_t node);
+static bst_iter_t GetMostLeftIMP(bst_iter_t node);
+static bst_iter_t DestroyAndGoUp(bst_iter_t node);
+
+static void ConnectLeftLegIMP(bst_iter_t parent, bst_iter_t node);
+static void ConnectRightLegIMP(bst_iter_t parent, bst_iter_t node);
 
 bst_t *BSTCreate(comparison_func func, void *param)
 {
@@ -51,23 +71,189 @@ bst_t *BSTCreate(comparison_func func, void *param)
 	}
 
 	new_tree->stub.data = (void *)0xDEADBEEF;
+	new_tree->stub.right = NULL;
+	new_tree->stub.left = NULL;
 	new_tree->comparison_func = func;
 	new_tree->comparison_param = param;
 	
 	return new_tree;
 }
-/*
+
 void BSTDestroy(bst_t *tree)
 {
-	bst_iter_t runner = tree->stub.left;
-	while (runner != NULL)
+	bst_iter_t runner = GetRootIMP(tree);
+	if (runner == NULL)
 	{
-		
-	
+		free(tree);
+		return;
 	}
-
+	
+	while (runner != &tree->stub)
+	{
+		switch (GetNumOfKidsIMP(runner))
+		{
+			case no_child:
+				runner = DestroyAndGoUp(runner);
+				break;
+			case left_child:
+				runner = GetDirectionIMP(runner, left);
+				break;
+			case right_child:
+				runner = GetDirectionIMP(runner, right);
+				break;
+			case two_childs:
+				runner = GetDirectionIMP(runner, right);
+				break;
+		}
+	}
+	free(tree);
 }
-*/
+
+
+bst_iter_t BSTNext(bst_iter_t iter)
+{
+	return BSTTraverseIMP(iter, 1);
+}
+
+bst_iter_t BSTPrev(bst_iter_t iter)
+{
+	return BSTTraverseIMP(iter, -1);
+}
+
+
+static bst_iter_t BSTTraverseIMP(bst_iter_t iter, int sign)
+{
+	bst_iter_t child = NULL;
+	
+	if (sign == 1)
+	{	
+		child = GetDirectionIMP(iter, right);
+		if (child != NULL)
+		{
+			
+			while (child->left != NULL)
+			{
+				child = GetDirectionIMP(child, left);
+			}
+			iter = child;
+		}
+		else
+		{
+			while (IsARightKidIMP(iter))
+			{
+				iter = GetDirectionIMP(iter, up);
+			}
+			iter = GetDirectionIMP(iter, up);
+		}
+	}
+	else
+	{
+		child = GetDirectionIMP(iter, left);
+		
+		if (child != NULL)
+		{
+			
+			while (child->right != NULL)
+			{
+				child = GetDirectionIMP(child, right);
+			}
+			iter = child;
+		}
+		else
+		{
+			while (!IsARightKidIMP(iter))
+			{
+				iter = GetDirectionIMP(iter, up);
+			}
+			iter = GetDirectionIMP(iter, up);
+		}	
+	}
+	return iter;
+}
+
+void BSTRemove(bst_iter_t iter)
+{
+	int is_right_child = IsARightKidIMP(iter);
+	bst_iter_t leftest = {0};
+	
+	switch (GetNumOfKidsIMP(iter))
+	{
+	case no_child:
+		if (is_right_child)
+		{
+			iter->parent->right = NULL;
+		}
+		else
+		{
+			iter->parent->left = NULL;
+		}
+		break;
+		
+	case left_child:
+		if (is_right_child)
+		{
+			ConnectRightLegIMP(iter->parent, iter->left);
+		}
+		else
+		{
+			ConnectLeftLegIMP(iter->parent, iter->left);
+		}
+		break;
+		
+	case right_child:
+		if (is_right_child)
+		{
+			ConnectRightLegIMP(iter->parent, iter->right);
+		}
+		else
+		{
+			ConnectLeftLegIMP(iter->parent, iter->right);
+		}
+		break;
+		
+	case two_childs:
+		leftest = GetMostLeftIMP(iter->right);
+		if (is_right_child)
+		{
+			ConnectRightLegIMP(iter->parent, iter->right);
+		}
+		else
+		{
+			ConnectLeftLegIMP(iter->parent, iter->right);
+		}
+		ConnectLeftLegIMP(leftest, iter->left);
+	}
+	free(iter);
+}
+
+size_t BSTSize(const bst_t *tree)
+{
+	bst_iter_t node = {0};
+	size_t counter = 0;
+	
+	if(BSTIsEmpty(tree) == 1)
+	{
+		return 0;
+	}
+	
+	for (node = BSTBegin(tree) ;
+		node != BSTEnd(tree) ;
+		node = BSTNext(node))
+	{
+		++counter;
+	}
+	return counter;
+}
+
+static int IsARightKidIMP(bst_iter_t node)
+{	
+	if (BSTIsSameIterator(node, GetDirectionIMP(node, up)->right))
+	{
+		return 1;
+	}
+	return 0;
+}
+
 
 void *BSTGetData(bst_iter_t iter)
 {
@@ -75,29 +261,29 @@ void *BSTGetData(bst_iter_t iter)
 }
 
 int BSTIsSameIterator(bst_iter_t iter1, bst_iter_t iter2)
-{
+{	
+	if (iter1 == NULL || iter2 == NULL)
+	{
+		return 0;
+	}
 	return (&iter1->data == &iter2->data);
 }
+
 
 bst_iter_t BSTBegin(const bst_t *tree)
 {
 	bst_iter_t node = GetRootIMP((bst_t *) tree);
-	while (node->left != NULL)
-	{
-		node = GetLeftIMP(node);
-	}
+	
+	node = GetMostLeftIMP(node);
 	
 	return node;
 }
+
 bst_iter_t BSTEnd(const bst_t *tree)
 {
-	bst_iter_t node = GetRootIMP((bst_t *) tree);
-	while (node->right != NULL)
-	{
-		node = GetRightIMP(node);
-	}
-	return node;
+	return (bst_iter_t)&tree->stub;
 }
+
 int BSTIsEmpty(const bst_t *tree)
 {
 	if ((size_t)&GetRootIMP((bst_t *) tree)->right >= 100)
@@ -111,6 +297,44 @@ int BSTIsEmpty(const bst_t *tree)
 	return 1;
 }
 
+bst_iter_t BSTFind(bst_t *tree, void *data)
+{
+	bst_iter_t node = {0};
+	int which_child = 0;
+	node = GetRootIMP(tree);
+	
+	while (NULL != node)
+	{
+		which_child = tree->comparison_func
+							(data, node->data, 
+							tree->comparison_param);
+		if (which_child == 0)
+		{
+			return node;
+		}
+		node = GetDirectionIMP(node, which_child);
+	}
+	return BSTEnd(tree);
+}
+
+int BSTForEach(bst_iter_t begin, bst_iter_t end, action_func func, void *param)
+{
+	bst_iter_t node = {0};
+	int status = 0;	
+	for (node = begin ;
+		node != end ;
+		node = BSTNext(node))
+		{
+			status = func(BSTGetData(node), param);
+			if (status != 0)
+			{
+				break;
+			}
+		}
+	return status;
+}
+
+
 bst_iter_t BSTInsert(bst_t *tree, void *data)
 {
 	bst_node_t *parent = &tree->stub;
@@ -118,7 +342,7 @@ bst_iter_t BSTInsert(bst_t *tree, void *data)
 	bst_node_t **new_node_parent_location = &(tree->stub.left);
 	int which_child = -1;
 	
-	while(NULL != runner)
+	while (NULL != runner)
 	{
 		parent = runner;
 		which_child = tree->comparison_func
@@ -140,36 +364,104 @@ bst_iter_t BSTInsert(bst_t *tree, void *data)
 	return *new_node_parent_location;
 }
 
-bst_node_t *CreateNodeIMP(bst_node_t *parent, void *data)
+static bst_node_t *CreateNodeIMP(bst_node_t *parent, void *data)
 {
 	bst_node_t *new_node = (bst_node_t *)malloc(sizeof(bst_node_t));
 	
 	new_node->parent = parent;
 	new_node->data = data;
+	new_node->left = NULL;
+	new_node->right = NULL;
 	
 	return new_node;
 }
 
-bst_node_t *GetRightIMP(bst_node_t *node)
-{
-	return node->right;
-}
 
-bst_node_t *GetLeftIMP(bst_node_t *node)
-{
-	return node->left;
-}
-
-bst_node_t *GetParentIMP(bst_node_t *node)
-{
-	return node->parent;
-}
-
-bst_iter_t GetRootIMP(bst_t *tree)
+static bst_iter_t GetRootIMP(bst_t *tree)
 {
 	return tree->stub.left;
 }
 
+static void ConnectLeftLegIMP(bst_iter_t parent, bst_iter_t node)
+{
+	parent->left = node;
+	node->parent = parent;
+}
+
+static void ConnectRightLegIMP(bst_iter_t parent, bst_iter_t node)
+{
+	parent->right = node;
+	node->parent = parent;
+}
+
+static enum kids_state GetNumOfKidsIMP(bst_iter_t node)
+{
+	if (GetDirectionIMP(node, left) != NULL && GetDirectionIMP(node, right) != NULL)
+	{
+		return two_childs;
+	}
+	if (GetDirectionIMP(node, left) != NULL)
+	{
+		return left_child;
+	}
+	if (GetDirectionIMP(node, right) != NULL)
+	{
+		return right_child;
+	}
+	
+	return no_child;
+}
 
 
+static bst_iter_t GetMostLeftIMP(bst_iter_t node)
+{
+	while (node->left != NULL)
+	{
+		node = GetDirectionIMP(node, left);
+	}
+	return node;	
+}
+
+
+static bst_node_t *GetDirectionIMP(bst_node_t *node, enum direction direction)
+{
+	bst_iter_t returned_iter = NULL;
+	
+	if (node == NULL)
+	{
+		return returned_iter;
+	}
+	
+	switch (direction)
+	{
+		case up:
+			returned_iter = node->parent;
+			break;
+		case right:
+			returned_iter = node->right;
+			break;
+		case left:
+			returned_iter = node->left;
+			break;
+	}
+	return returned_iter;
+}
+
+static bst_iter_t DestroyAndGoUp(bst_iter_t node)
+{
+	bst_iter_t parent = NULL;
+	parent = GetDirectionIMP(node, up);
+	
+	if (IsARightKidIMP(node))
+	{
+		parent->right = NULL;
+	}
+	else
+	{
+		parent->left = NULL;
+	}
+	free(node);
+	
+	return parent;
+}
 
--- ds/bst/bst_test.c
+++ ds/bst/bst_test.c
@@ -36,62 +36,136 @@ int my_cmp (const void *new_data,
 							   const void *src_data, 
 							   const void *comp_param);
 
-void TestBSTCreate();
+void TestBSTRemove();
 void TestBSTInsert();
+void TestBSTFind();
+void TestBSTForEach();
+void TestBSTDestroy();
+static int my_action(void *data, void *for_each_param);
 
 int main()
 {
-TestBSTCreate();
-TestBSTInsert();
+
+	TestBSTInsert();
+	TestBSTRemove();
+	TestBSTFind();
+	TestBSTForEach();
+	TestBSTDestroy();
+	
+	return 0;
 }
 
-void TestBSTCreate()
+static int my_action(void *data, void *for_each_param)
 {
-	int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int i = 0;
-	bst_t *my_tree = NULL;
-	bst_iter_t my_iter[10] = {0};
-	bst_iter_t new_iter = {0};
+	if (*(int*)data > *(int*)for_each_param)
+	{
+		return 1;
+	}
 
-	my_tree = BSTCreate(my_cmp, NULL);
-<<<<<<< HEAD
-	/*my_iter = BSTInsert(my_tree, &x);*/
-	
-	
+	*(int*)data *= 2;
+
+	return 0;
 }
 
+void TestBSTDestroy()
+{
+	int arr[10] = {5, 1, 2, 8, 3, 7, 4, 6, 9, 0};
+	int i = 0;
+	int action_param = 8;
+		
+	bst_t *tree = BSTCreate(my_cmp, NULL);
+	bst_iter_t my_iter[10] = {0};
+	for (i = 0 ; i < 10 ; i++)
+	{
+		my_iter[i] = BSTInsert(tree, &arr[i]);
+	}
+	BSTDestroy(tree);
+
+
+}
+
+void TestBSTForEach()
+{
+	int arr[10] = {5, 1, 2, 8, 3, 7, 4, 6, 9, 0};
+	int i = 0;
+	int action_param = 8;
+		
+	bst_t *tree = BSTCreate(my_cmp, NULL);
+	bst_iter_t my_iter[10] = {0};
+	for (i = 0 ; i < 10 ; i++)
+	{
+		my_iter[i] = BSTInsert(tree, &arr[i]);
+	}
+	TEST("ForEach", BSTForEach(BSTBegin(tree), BSTEnd(tree), my_action, &action_param) == 1);
+	action_param = 20;
+	TEST("ForEach", BSTForEach(BSTBegin(tree), BSTEnd(tree), my_action, &action_param) == 0);
+	TEST("Find and ForEach", BSTFind(tree, &arr[2]) == my_iter[2]);
+	BSTDestroy(tree);
+}
+
+void TestBSTFind()
+{
+	int arr[10] = {5, 1, 2, 8, 3, 7, 4, 6, 9, 0};
+	int i = 0;
+		
+	bst_t *tree = BSTCreate(my_cmp, NULL);
+	bst_iter_t my_iter[10] = {0};
+	for (i = 0 ; i < 10 ; i++)
+	{
+		my_iter[i] = BSTInsert(tree, &arr[i]);
+	}
+	TEST("Find", BSTFind(tree, &arr[0]) == my_iter[0]);
+	TEST("Find", BSTFind(tree, &i) == BSTEnd(tree));
+		BSTDestroy(tree);
+}
+
+void TestBSTRemove()
+{
+	int arr[10] = {5, 1, 2, 8, 3, 7, 4, 6, 9, 0};
+	int i = 0;
+		
+	bst_t *tree = BSTCreate(my_cmp, NULL);
+	bst_iter_t my_iter[10] = {0};
+	TEST("Remove Size", BSTSize(tree) == 0);
+	for (i = 0 ; i < 10 ; i++)
+	{
+		my_iter[i] = BSTInsert(tree, &arr[i]);
+	}
+	
+	BSTRemove(my_iter[1]);
+	TEST("Remove", *(int*)BSTGetData(BSTNext(BSTBegin(tree))) == arr[2]);
+	BSTRemove(my_iter[5]);
+	TEST("Remove", *(int*)BSTGetData(BSTPrev(BSTPrev(BSTPrev(BSTEnd(tree))))) == arr[7]);
+	BSTRemove(my_iter[9]);
+	TEST("Remove", *(int*)BSTGetData(BSTBegin(tree)) == arr[2]);
+	BSTRemove(my_iter[3]);
+	TEST("Remove", *(int*)BSTGetData(BSTPrev(BSTPrev(BSTPrev(BSTEnd(tree))))) == arr[0]);
+	BSTRemove(my_iter[0]);
+	TEST("Remove", *(int*)BSTGetData(BSTNext(BSTBegin(tree))) == arr[4]);
+	TEST("Remove Size", BSTSize(tree) == 5);
+		BSTDestroy(tree);
+}
 
 void TestBSTInsert()
 {
-	int x = 10;
-	int y = 5;
-	int z = 2;
-
+	int arr[10] = {5, 1, 2, 8, 3, 7, 4, 6, 9, 0};
+	int i = 0;
+		
 	bst_t *tree = BSTCreate(my_cmp, NULL);
-	bst_node_t *node1 = BSTInsert(tree, &x);
+	bst_iter_t my_iter[10] = {0};
 
-
-	TEST("TestBSTInsert", (*(int*)(tree -> stub.left-> data) == 10));
-	printf("%d \n",*(int*)(tree -> stub.left-> data));
-
-	BSTInsert(tree, &y);
-	printf("%d \n",*(int*)(tree -> stub.left->left-> data));
-
-	BSTInsert(tree, &z);
-	printf("%d \n",*(int*)(tree -> stub.left->left->left-> data));
-
-=======
-	printf("is it empty? %d\n",BSTIsEmpty(my_tree));
 	for (i = 0 ; i < 10 ; i++)
 	{
-		my_iter[i] = BSTInsert(my_tree, &arr[i]);
+		my_iter[i] = BSTInsert(tree, &arr[i]);
 	}
-	printf("is it empty? %d\n",BSTIsEmpty(my_tree));
 	
-	new_iter = BSTEnd(my_tree);
-	printf("%d",BSTIsSameIterator(new_iter, my_iter[9]));
-	printf("is it the start? %d",BSTIsSameIterator(BSTBegin(my_tree), my_iter[0]));
->>>>>>> 5b9c9522cceec75e506caf4db50e05f6abeb9e22
+	TEST("First Element", *(int*)BSTGetData(my_iter[9]) == 
+							*(int*)BSTGetData(BSTBegin(tree)));
+	TEST("Third Element", *(int*)BSTGetData(my_iter[2]) == 
+							*(int*)BSTGetData(BSTNext(BSTNext(BSTBegin(tree)))));		
+	TEST("Last Element", *(int*)BSTGetData(my_iter[8]) ==
+						 *(int*)BSTGetData(BSTPrev(BSTEnd(tree))));
+ 	BSTDestroy(tree);
 }
 
 
@@ -99,6 +173,14 @@ int my_cmp (const void *new_data,
 							   const void *src_data, 
 							   const void *comp_param)
 {
-	return *(int *)new_data - *(int*)src_data;
+	if (*(int *)new_data > *(int*)src_data)
+	{
+		return 1;
+	}
+	else if (*(int *)new_data < *(int*)src_data)
+	{
+		return -1;
+	}
+	return 0;
 
 }
