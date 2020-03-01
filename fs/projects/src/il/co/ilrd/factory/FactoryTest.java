c0ff87a004599c1231e1853f600f99a074ee6b99
aviv.duek <aviv.duek@gmail.com>
2020-03-01 16:14:27 +0200

    d

--- fs/projects/src/il/co/ilrd/factory/FactoryTest.java
+++ fs/projects/src/il/co/ilrd/factory/FactoryTest.java
@@ -1,114 +1,147 @@
 package il.co.ilrd.factory;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Map;
 import java.util.function.Function;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import il.co.ilrd.hashMap.HashMap;
 
-
-
-class FactoryTest {
+public class FactoryTest<T, K, D> {
+	private Map<K, Function<D, ? extends T>> map = 
+								new HashMap<K, Function<D, ? extends T>>();
 	
-	private static class fac {
-		public Animal animalcreate(Object arg0) {
-			return new FactoryTest().new Animal();
-		}
-		static public Creature animalcreatestatic(Object arg0) {
-			return new FactoryTest().new Animal();
-		}
+	public void add(K key, Function<D, ? extends T> func) {
+		map.put(key,func);
 	}
-	class Creature{
+
+	public T create(K key, D data) {
+		return map.get(key).apply(data);
+	}
+	
+	public T create(K key) {
+		return map.get(key).apply(null);
+	}
+}
+
+
+
+class Main{
+	public static void main(String[] args) {
+		FactoryTest<Creature, Integer, Creature> factor = new FactoryTest<>();
 		
-		public Creature creating() {
-			return new Creature();
-		}
+		// Lambda
+		Function<Creature, Animal> animalCreate = (animal)-> new Animal();
+		factor.add(1, animalCreate);
+		Creature animal = factor.create(1);
+		System.out.println(animal.breath());
 		
-		String breath() {
-			return "air in air out";
-		}
-		String makeNoise() {
-			return "basic creature sound";
-		}
-	}
-	
-	class Animal extends Creature{
+		Function<Creature, Alien> alienCreate = (alien)-> new Alien();
+		factor.add(2, alienCreate);
+		Creature alien = factor.create(2);
+		System.out.println(alien.breath());
+		
+		
+		// Anunymos Class.
+		Function<Creature, Animal> func1 = new Function<Creature, Animal>(){
 
-		@Override
-		String breath() {
-			return "earth air in earth air out";
-		}
+			@Override
+			public Animal apply(Creature arg0) {
+				return new Animal();
+			}
+		};
+		factor.add(3, func1);
+		Creature animal2 = factor.create(3);
+		System.out.println(animal2.makeNoise());
+		
+		Function<Creature, Alien> func2 = new Function<Creature, Alien>(){
 
-		@Override
-		String makeNoise() {
-			return "the sound of life";
-		}
-	}
-	
-	class Alien extends Creature{
-		@Override
-		String breath() {
-			return "space air in space air out";
-		}
-
-		@Override
-		String makeNoise() {
-			return "the sound of weird life";
-		}
-	}
-	
-	
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void testCreateKD() {
-//		Factory<Creature, Integer, Object> creatureFactory = new Factory<>();
-//		Function<Object, Animal> createAnimal = (animal) -> new Animal();
-//		creatureFactory.add(0, createAnimal);
-//		assertEquals("earth air in earth air out", creatureFactory.create(0).breath());
-//		creatureFactory.add(1, new Function<Object, Animal>(){
-//
-//			@Override
-//			public Animal apply(Object arg0) {
-//				return new Animal();
-//			}
-//			
-//			}
-//		);
-//		assertEquals("earth air in earth air out", creatureFactory.create(1).breath());
-//		creatureFactory.add(11,new fac()::animalcreate);
-//		System.out.println(creatureFactory.create(11).breath());
-//		creatureFactory.add(12,fac::animalcreatestatic);
-//		System.out.println(creatureFactory.create(12).breath());
-//		creatureFactory.create(666, new Animal()).breath();
-		Factory<Creature, Integer, Creature> newcreatureFactory = new Factory<>();
-		newcreatureFactory.add(1, Creature::creating);
-		System.out.println(newcreatureFactory.create(1, new Creature()).breath());
-
+			@Override
+			public Alien apply(Creature arg0) {
+				return new Alien();
+			}
+		};
+		factor.add(4, func2);
+		Creature alien2 = factor.create(4);
+		System.out.println(alien2.makeNoise());
+		
+		
+		// Static method.
+		factor.add(5, Animal::cAnimal);
+		Creature aniaml3 = factor.create(5);
+		System.out.println(aniaml3.breath());
+		
+		// Instance method.
+		Alien realAlien = new Alien();
+		factor.add(6, realAlien::cAlien);
+		Creature alien3 = factor.create(6);
+		System.out.println(alien3.breath());
+		
+		
+		
+		// Instance Method without instnace
+		factor.add(7, Creature::ctorAlien);
+		Creature alien4 = factor.create(7, new Creature());
+		System.out.println(alien4.breath());
+		
+//		StaticMethod obj = new StaticMethod();
+//		factor.add(8, StaticMethod::cAlien);
+//		Creature alien4 = factor.create(8);
+//		System.out.println(alien4.makeNoise());
 		
 	}
-	
-	@Test
-	void testCreateK() {
-		fail("Not yet implemented");
+}
+
+class StaticMethod{
+	public Alien cAlien() {
+		return new Alien();
 	}
+	
+}
+
+
+class Creature{
+	String breath() {
+		return "Creature breath";
+	}
+	String makeNoise() {
+		return "Creature makeNoise";
+	}
+	
+	public Alien ctorAlien() {
+		return new Alien();
+	}
+}
+	
+class Animal extends Creature{
+	
+	@Override
+	String breath() {
+		return "Animal breath";
+	}
+	
+	@Override
+	String makeNoise() {
+		return "Animal makeNoise";
+	}
+	
+	public static Animal cAnimal(Creature i) {
+		return new Animal();
+	}
+}
+	
+class Alien extends Creature{
+	@Override
+	String breath() {
+		return "Alien breath";
+	}
+	
+	@Override
+	String makeNoise() {
+		return "Alien makeNoise";
+	}
+	
+	public Alien cAlien(Creature i) {
+		return new Alien();
+	}
+	
 
 }
